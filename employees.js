ab7cc0c98a7729e7bd954a39c697bbadcccc3f71
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-18 12:47:36 +0530

    calculate wages

--- employees.js
+++ employees.js
@@ -6,3 +6,24 @@ if(emp_check == is_absent){
 else{
     console.log("Employee is present")
 }
+
+const is_part_time =1;
+const is_full_time = 2;
+
+const part_time_hours = 4;
+const full_time_hours = 8;
+const wages_per_hour = 20;
+
+emp_check = Math.floor(Math.random()*10) % 3;
+switch(emp_check){
+    case is_part_time:
+        empHrs = part_time_hours;
+        break;
+    case is_full_time:
+        empHrs = full_time_hours;
+        break;
+    default:
+        empHrs = 0;    
+}
+let empWage = empHrs * wages_per_hour;
+console.log(empWage);
