77db5f477ac039a47d93acb02eb6acfd170d5db4
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-18 12:36:49 +0530

    initial commit

---
+++ employees.js
@@ -0,0 +1,8 @@
+const is_absent =0;
+let emp_check = Math.floor(Math.random)*10 %2;
+if(emp_check == is_absent){
+    console.log("Employee is absent");
+}
+else{
+    console.log("Employee is present")
+}
